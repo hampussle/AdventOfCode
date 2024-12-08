749efa4bcbb3fbc0d66943b2d942452f664b1e63
Hampus Linderstam <hlinderstam@gmail.com>
2024-12-08 21:51:28 +0100

    Fixed input newline split

--- Helpers/Day.cs
+++ Helpers/Day.cs
@@ -17,7 +17,7 @@ public abstract class Day
     public bool UseTestInput { get; set; }
     public string Input => UseTestInput ? InputHandler.GetTestInput(YearNumber, DayNumber)
                                         : InputHandler.GetInputAsync(YearNumber, DayNumber).Result;
-    public string[] SplitInput => [.. Input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)];
+    public string[] SplitInput => [.. Input.ReplaceLineEndings().Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)];
 
     public virtual string PartOne() => "no answer";
     public virtual string PartTwo() => "no answer";
