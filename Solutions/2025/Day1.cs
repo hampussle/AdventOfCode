78826893453e40c4ec3c281e720b70c5de8f7d33
Hampus Linderstam <hlinderstam@gmail.com>
2025-12-01 06:19:42 +0100

    Day 1 2025

--- Solutions/2025/Day1.cs
+++ Solutions/2025/Day1.cs
@@ -7,12 +7,43 @@ public class Day1(int year, int day) : Day(year, day)
 
     public override string PartOne()
     {
-        return base.PartOne();
+        int dial = 50;
+        int hitZero = 0;
+        foreach (var line in SplitInput)
+        {
+            bool isRight = line[0] == 'R';
+            int steps = int.Parse(line[1..]);
+            dial += isRight ? steps : -steps;
+            while (dial < 0)
+                dial += 100;
+            while (dial > 99)
+                dial -= 100;
+            if (dial == 0)
+                hitZero++;
+        }
+        return hitZero.ToString();
     }
 
     public override string PartTwo()
     {
-        return base.PartTwo();
+        int dial = 50;
+        int hitZero = 0;
+        foreach (var line in SplitInput)
+        {
+            int steps = int.Parse(line[1..]);
+            int step = line[0] == 'R' ? 1 : -1;
+            for (int i = 0; i < steps; i++)
+            {
+                dial += step;
+                if (dial == 100)
+                    dial = 0;
+                if (dial == -1)
+                    dial = 99;
+                if (dial == 0)
+                    hitZero++;
+            }
+        }
+        return hitZero.ToString();
     }
 
 }
