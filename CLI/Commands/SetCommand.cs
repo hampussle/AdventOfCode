614f7fe1be5cdf02a5f12a3e2e9096e5d4848baa
Hampus Linderstam <hlinderstam@gmail.com>
2025-12-01 06:19:32 +0100

    Update set command, will move apikey handling to its own command

--- CLI/Commands/SetCommand.cs
+++ CLI/Commands/SetCommand.cs
@@ -9,26 +9,26 @@ internal class SetCommand : Command<SetCommand.Settings>
 {
     public sealed class Settings : CommandSettings
     {
-        [Description("Set the API key used for fetching input.")]
-        [CommandOption("-a|--api [APIKEY]")]
-        public FlagValue? ApiKey { get; set; }
+        //[Description("Set the API key used for fetching input.")]
+        //[CommandOption("-a|--api <APIKEY>")]
+        //public string? ApiKey { get; init; }
 
-        [Description("Set the test input to use when using command <run -t>")]
-        [CommandOption("-t|--testinput [TESTINPUT]")]
-        public FlagValue? TestInput { get; init; }
+        [Description("Set the test input to use when running command <run -t>")]
+        [CommandArgument(2, "<TESTINPUT>")]
+        public string? TestInput { get; init; }
 
         [Description("Year (2015-2025)")]
-        [CommandArgument(0, "[YEAR]")]
+        [CommandArgument(0, "<YEAR>")]
         public int? Year { get; init; }
 
         [Description("Day (1-25)")]
-        [CommandArgument(1, "[DAY]")]
+        [CommandArgument(1, "<DAY>")]
         public int? Day { get; init; }
 
         public override ValidationResult Validate()
         {
-            if (ApiKey is null && TestInput is null)
-                return ValidationResult.Error("Either --api or --testinput must be provided.");
+            //if (ApiKey is null && TestInput is null)
+            //    return ValidationResult.Error("Either --api or --testinput must be provided.");
 
             if (TestInput is not null)
                 if (Year is null || Day is null)
@@ -40,35 +40,20 @@ internal class SetCommand : Command<SetCommand.Settings>
 
     public override int Execute(CommandContext context, Settings settings, CancellationToken cancellationToken)
     {
-        if (settings.ApiKey?.IsSet ?? false)
-        {
-            if (settings.ApiKey.Value is string apiKey)
-            {
-                var path = InputHandler.SetApiKey(apiKey);
-                AnsiConsole.MarkupLine("[green]API key set[/]");
-                AnsiConsole.WriteLine();
-                AnsiConsole.MarkupLine($"[red]Key stored in:[/] {path}");
-            }
-        }
+        //if (settings.ApiKey is string apiKey)
+        //{
+        //    var path = InputHandler.SetApiKey(apiKey);
+        //    AnsiConsole.MarkupLine("[green]API key set[/]");
+        //    AnsiConsole.WriteLine();
+        //    AnsiConsole.MarkupLine($"[red]Key stored in:[/] {path}");
+        //}
 
-        if (settings.TestInput?.IsSet ?? false)
+        if (settings.TestInput is string testInput && settings.Year is int year && settings.Day is int day)
         {
-            if (settings.TestInput?.Value is string testInput && settings.Year is int year && settings.Day is int day)
-            {
-                InputHandler.WriteTestInput(testInput, year, day);
-                AnsiConsole.MarkupLine($"[green]Test input set for {year} - {day}.[/]");
-            }
+            InputHandler.WriteTestInput(testInput, year, day);
+            AnsiConsole.MarkupLine($"[green]Test input set for {year} - {day}.[/]");
         }
 
         return 0;
     }
-}
-
-class FlagValue : IFlagValue
-{
-    public bool IsSet { get; set; }
-
-    public Type Type => typeof(string);
-
-    public object? Value { get; set; }
 }
