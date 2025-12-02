ef1685f63829c8e4c3836a2dca4bf59fc0418f45
Hampus Linderstam <hlinderstam@gmail.com>
2025-12-02 07:09:57 +0100

    Day 2 2025

--- Solutions/2025/Day2.cs
+++ Solutions/2025/Day2.cs
@@ -7,12 +7,55 @@ public class Day2(int year, int day) : Day(year, day)
 
     public override string PartOne()
     {
-        return base.PartOne();
+        var ranges = Input.Split(',');
+        long result = 0;
+
+        foreach (var range in ranges)
+        {
+            var ids = range.Split('-');
+            var start = long.Parse(ids[0]);
+            var end = long.Parse(ids[1]);
+            for (long i = start; i <= end; i++)
+            {
+                var nrStr = i.ToString();
+                var firstPart = nrStr[..(nrStr.Length / 2)];
+                var secondPart = nrStr[(nrStr.Length / 2)..];
+                if (firstPart.Equals(secondPart))
+                    result += i;
+            }
+        }
+
+        return result.ToString();
     }
 
     public override string PartTwo()
     {
-        return base.PartTwo();
+        var ranges = Input.Split(',');
+        long result = 0;
+
+        foreach (var range in ranges)
+        {
+            var ids = range.Split('-');
+            var start = long.Parse(ids[0]);
+            var end = long.Parse(ids[1]);
+
+            for (long i = start; i <= end; i++)
+            {
+                var nrStr = i.ToString();
+                string curr = "";
+                foreach (var c in nrStr)
+                {
+                    curr += c.ToString();
+                    if (curr.Length < nrStr.Length && string.IsNullOrWhiteSpace(nrStr.Replace(curr, string.Empty)))
+                    {
+                        result += i;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return result.ToString();
     }
 
 }
