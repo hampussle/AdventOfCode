94877e955e2ceb18f89a0cf4775168e58ab8f550
Hampus Linderstam <hlinderstam@gmail.com>
2025-12-06 09:17:06 +0100

    Day 6 2025
    Part 2 WIP

--- Solutions/2025/Day6.cs
+++ Solutions/2025/Day6.cs
@@ -1,4 +1,5 @@
 using Helpers;
+using System.Runtime.CompilerServices;
 
 namespace Solutions.Year2025;
 
@@ -7,12 +8,59 @@ public class Day6(int year, int day) : Day(year, day)
 
     public override string PartOne()
     {
-        return base.PartOne();
+        var normalizedInput = SplitInput.Select(line => line.NormalizeWhitespace().Replace(" ", ",")).ToArray();
+        var operators = normalizedInput.Last().Split(',', StringSplitOptions.RemoveEmptyEntries);
+        normalizedInput = [.. normalizedInput.Take(normalizedInput.Length - 1)];
+        var numberRows = normalizedInput.Select(line => line.Split(',', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray()).ToArray();
+
+        long total = 0L;
+        var maxCol = numberRows.Max(row => row.Length);
+        for (int col = 0; col < maxCol; col++)
+        {
+            var op = operators[col];
+            long mul = 1L;
+            long add = 0L;
+            foreach (var row in numberRows)
+            {
+                if (op == "*")
+                    mul *= row[col];
+                else
+                    add += row[col];
+            }
+            if (op == "*")
+                total += mul;
+            else
+                total += add;
+        }
+
+        return total.ToString();
     }
 
     public override string PartTwo()
     {
-        return base.PartTwo();
+        List<List<int>> numbers = [];
+        int outerColumn = 0;
+        numbers.Add([]);
+        for (int col = SplitInput.Max(line => line.Length) - 1; col >= 0; col--)
+        {
+            if (SplitInput.All(line => line.Length <= col || line[col] == ' '))
+            {
+
+            }
+            string number = "";
+            for (int row = 0; row < SplitInput.Length; row++)
+            {
+                string? line = SplitInput[row];
+                if (line.Length <= col)
+                    continue;
+                var c = line[col];
+                if (c == ' ')
+                    continue;
+                numbers[row].Add(int.Parse(c.ToString()));
+            }
+            if (!string.IsNullOrWhiteSpace(number))
+                numbers[outerColumn].Add(int.Parse(number));
+        }
     }
 
 }
